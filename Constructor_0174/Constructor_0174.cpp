c105cbbfe1b0b2ae0eae77443ae46c35fd6087c0
thekill022 <gibfathoni@gmail.com>
2024-05-14 13:53:46 +0700

    mendefinisikan prosedr - prosedur di luar class

--- Constructor_0174/Constructor_0174.cpp
+++ Constructor_0174/Constructor_0174.cpp
@@ -24,6 +24,22 @@ public:
 //memberi value ke variabel nim dari class mahasiswa
 int Mahasiswa::nim = 174;
 
+//mendefinisikan prosedr - prosedur di luar class
+void Mahasiswa::setID() {
+
+	id = ++nim;
+
+};
+
+void Mahasiswa::printAll() {
+
+	cout << "ID : " << id << endl;
+	cout << "Nama : " << nama << endl;
+	cout << endl;
+
+};
+
+
 int main() {
 
 }
