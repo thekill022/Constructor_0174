5e43de07150f6fbf55a7991bd6e9824e81ff739e
thekill022 <gibfathoni@gmail.com>
2024-05-14 14:00:09 +0700

    pembuatan dan pendefinisian constructor yang di definiskan didalam class

--- Constructor(3)_0174/Constructor(3)_0174.cpp
+++ Constructor(3)_0174/Constructor(3)_0174.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	//pembuatan dan pendefinisian constructor yang di definiskan didalam class
+	Mahasiswa() {
+		nim = 174;
+		nama = "Dzikri";
+	};
+
+	Mahasiswa(int iNim) {
+		nim = iNim;
+	};
+}
+
 int main() {
 
 }
